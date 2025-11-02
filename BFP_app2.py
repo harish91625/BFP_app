50ce44b76302135556e829dcd7c9d24c502c3f16
harish91625 <harish.eee25@gmail.com>
2025-11-02 21:47:57 +0530

    Update BFP_app2.py

--- BFP_app2.py
+++ BFP_app2.py
@@ -2,16 +2,18 @@ import streamlit as st
 import joblib
 import numpy as np
 
-bfp_model= joblib.load("bfp_eff_design.sav")
-st.title("Design BFP efficiency")
-st.write("Enter the BFP flow to know the design BFP Efficiency")
+bfp_model= joblib.load(r"C:\Users\H&S PC\bfp_eff_design.sav")
+bfp_model1= joblib.load(r"C:\Users\H&S PC\bfp_head_design.sav")
+st.title("Design BFP EFFICIENCY and HEAD")
+st.write("Enter the BFP flow to know the design BFP Efficiency and BFP HEAD")
 
 flow_value= st.number_input("Enter Flow:",min_value=0.0, max_value=1000.0, value=100.0, step=1.0)
 
-if st.button("Design BFP Efficiency"):
+if st.button("Design BFP Efficiency and Head"):
     input_array= np.array([[flow_value]])
     output= bfp_model.predict(input_array)
-    st.success(f"Design BFP Efficiency for the flow of {flow_value} is : **{output[0]:.2f}%**")
-
+    output1=bfp_model1.predict(input_array)
+    st.success(f"Design BFP Efficiency and Head for the test flow of {flow_value} is : **{output[0]:.2f}%** and {output1[0]:.2f}")
 
 st.info("This model uses Random Forest to predict BFP efficiency based on flow rate")
+
