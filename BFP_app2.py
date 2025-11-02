3feff2afdee1fa623d5f8d05860d756fe3171c4d
harish91625 <harish.eee25@gmail.com>
2025-11-02 19:35:03 +0530

    Update BFP_app2.py

--- BFP_app2.py
+++ BFP_app2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import numpy as np
 
-bfp_model= joblib.load(r"C:\Users\H&S PC\bfp_eff_design.sav")
+bfp_model= joblib.load("bfp_eff_design.sav")
 st.title("Design BFP efficiency")
 st.write("Enter the BFP flow to know the design BFP Efficiency")
 
@@ -12,5 +12,6 @@ if st.button("Design BFP Efficiency"):
     input_array= np.array([[flow_value]])
     output= bfp_model.predict(input_array)
     st.success(f"Design BFP Efficiency for the flow of {flow_value} is : **{output[0]:.2f}%**")
+
 
 st.info("This model uses Random Forest to predict BFP efficiency based on flow rate")
